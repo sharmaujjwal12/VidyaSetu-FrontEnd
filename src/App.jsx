d3ff46f7a89c06ef8010cd072300232c357184e4
Ujjwal Sharma <us3566650@gmail.com>
2026-04-03 11:12:56 +0530

    Updated frontend changes

--- src/App.jsx
+++ src/App.jsx
@@ -850,7 +850,7 @@ function App() {
               role={role}
             />
           )}
-          <div className="flex-1">
+          <div className="flex-1 flex flex-col min-h-screen">
             <NavBar
               login={login}
               signup={signup}
